5c9e5cab6cc32cdb6ac44554ac9c693dd3fb6659
Ed <alpezed@gmail.com>
2023-02-15 10:32:27 +0800

    add missing add event ui

--- app/page.js
+++ app/page.js
@@ -1,5 +1,21 @@
 import Todos from "../components/todos";
 
-export default function Home() {
-	return <Todos />;
+async function Home() {
+	return (
+		<div className='row d-flex justify-content-center container mx-auto'>
+			<div className='col-md-8'>
+				<div className='card-hover-shadow-2x mb-3 card'>
+					<div className='card-header-tab card-header'>
+						<div className='card-header-title font-size-lg text-capitalize font-weight-normal'>
+							<i className='fa fa-calendar' />
+							&nbsp;Event Lists
+						</div>
+					</div>
+					<Todos />
+				</div>
+			</div>
+		</div>
+	);
 }
+
+export default Home;
--- components/todos.jsx
+++ components/todos.jsx
@@ -43,45 +43,29 @@ const Todos = () => {
 
 	return (
 		<>
-			<div className='row d-flex justify-content-center container mx-auto'>
-				<div className='col-md-8'>
-					<div className='card-hover-shadow-2x mb-3 card'>
-						<div className='card-header-tab card-header'>
-							<div className='card-header-title font-size-lg text-capitalize font-weight-normal'>
-								<i className='fa fa-calendar' />
-								&nbsp;Event Lists
-							</div>
-						</div>
-						{todos.length > 0 ? (
-							<TodoList
-								todos={todos}
-								onAdd={addNewTodoHandler}
-								onRemoveTodo={removeTodoHandler}
-								setEditingItem={setEditingItem}
-							/>
-						) : (
-							<div className='p-5 text-center text-muted opacity-25'>
-								<div>
-									<span className='d-block mb-2'>
-										<i className='fa fa-calendar fs-1'></i>
-									</span>
-									<h2
-										className='h3 text-muted'
-										style={{ "--bs-text-opacity": 0.5 }}
-									>
-										Start adding an event
-									</h2>
-								</div>
-							</div>
-						)}
-
-						<div className='d-block text-end card-footer'>
-							<Button variant='primary' onClick={handleShow}>
-								+ Add Event
-							</Button>
-						</div>
+			{todos.length > 0 ? (
+				<TodoList
+					todos={todos}
+					onAdd={addNewTodoHandler}
+					onRemoveTodo={removeTodoHandler}
+					setEditingItem={setEditingItem}
+				/>
+			) : (
+				<div className='p-5 text-center text-muted opacity-25'>
+					<div>
+						<span className='d-block mb-2'>
+							<i className='fa fa-calendar fs-1'></i>
+						</span>
+						<h2 className='h3 text-muted' style={{ "--bs-text-opacity": 0.5 }}>
+							Start adding an event
+						</h2>
 					</div>
 				</div>
+			)}
+			<div className='d-block text-end card-footer'>
+				<Button variant='primary' onClick={handleShow}>
+					+ Add Event
+				</Button>
 			</div>
 			<CreateTodoModal
 				mode='add'
